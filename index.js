025ea5b4872990b25aeed6a01f08b3ee36a57112
cauesantg <caue_paraty@hotmail.com>
2022-07-14 12:18:08 -0300

    DOM inicio

--- index.js
+++ index.js
@@ -1,51 +1,34 @@
-const productsCart = [
-    {
-        id: 1,
-        name: "Uva Crimson",
-        price: 44.99
-    },
-    {
-        id: 2,
-        name: "Vinho coração",
-        price: 17.98
-    },
-    {
-        id: 3,
-        name: "Água de coco",
-        price: 8.99
-    },
-    {
-        id: 4,
-        name: "Mamão",
-        price: 9.98
-    },
-    {
-        id: 5,
-        name: "Água tônica",
-        price: 17.98
-    }
+//referenciando pai dinamico
+const CorpoPagina = document.querySelector("main")
+const produtoLista = document.querySelector("ul")
+const rodape = document.querySelector("section")
+
+function criaCarrinho(){
     
-]
+    const tagHeader = document.createElement("header")
+    const tagH1 = document.createElement("h1")
+    const tagDiv = document.createElement("div")
+    const tagH2 = document.createElement("h2")
+    const tagH3 = document.createElement("h3")
 
-//Percorrendo produtos
-const listaCarrinho = document.querySelector("ul")
-
-function listarProduto(listaProduto){
-
-    for (let i = 0; i < listaProduto.length; i++){
-        const produto = listaProduto[i]
-        
-        const lista = criarListaProdutos(produto)
-        
-        listaCarrinho.appendChild(lista)
-    }
-
+    tagH1.classList.add("titulo")
+    tagH1.innerText = "Virtual Market"
+    tagDiv.classList.add("cabecalho")
+    tagH2.innerText = "Item"
+    tagH3.innerText = "Valor"
+   
+    CorpoPagina.appendChild(tagHeader)
+    tagHeader.appendChild(tagH1)
+    tagHeader.appendChild(tagDiv)
+    tagDiv.appendChild(tagH2)
+    tagDiv.appendChild(tagH3)
+ 
+    return tagHeader
 }
-listarProduto(productsCart)
+criaCarrinho()
 
-
-//Criando Lista
-function criarListaProdutos (produto){
+//Criando produto
+function criaLista (produto){
 
 //referenciar obj
     const nome = produto.name
@@ -70,4 +53,73 @@ function criarListaProdutos (produto){
 //retorna li pronto
     return tagLi
 }
+
+//Percorrendo produtos no array
+function listarProduto(prod){
+
+    for (let i = 0; i < prod.length; i++){
+        const produto = prod[i]
+        
+        const lista = criaLista(produto)
+        
+        produtoLista.appendChild(lista)
+    
+
+    }
+
+}//chamando funcao listarProduto
+listarProduto(productsCart)
+
+function finalizarCompra (){
+
+    
+    const tagDiv2 = document.createElement("div")
+    const tagP1 = document.createElement("p")
+    const tagBtn = document.createElement("button")
+
+    tagDiv2.classList.add("divSoma")
+    tagP1.innerText = "Total:"
+    tagBtn.classList.add("btnComprar")
+    tagBtn.innerText = "Finalizar Compra"
+    
+    rodape.appendChild(tagDiv2)
+    tagDiv2.appendChild(tagP1)
+    rodape.appendChild(tagBtn)
+    
+    return tagDiv2
+}
+finalizarCompra()
+
+const totalcompra = document.querySelector(".divSoma")
+
+function somarProdutos(prod){
+    let total = 0
+    for (let i = 0; i < prod.length; i++){
+        
+        total += prod[i].price
+        
+    }
+    const tagP2 = document.createElement("p")
+    tagP2.innerText = total
+    rodape.appendChild(totalcompra)
+    totalcompra.appendChild(tagP2)
+
+}
+
+somarProdutos(productsCart)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
